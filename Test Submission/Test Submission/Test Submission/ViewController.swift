0620365c45c9933eb365c41d4b7f1e25348e1ac5
florywong <FloraWong@Wong-Flora-Sin-Tings-MacBook-Air.local>
2015-06-03 21:45:42 +0800

    test submission

---
+++ Test Submission/Test Submission/Test Submission/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test Submission
+//
+//  Created by Flora Wong on 6/3/15.
+//  Copyright (c) 2015 Flora Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
